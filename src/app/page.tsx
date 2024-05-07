176c8722aa756916a6b6a457b94732b50f39991d
Brandon McConathy <brandonmcconathy4321@gmail.com>
2024-05-06 18:05:12 -0700

    Styled new link input field

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,23 @@
+'use client'
+
 import Link from "next/link";
+import { useState } from "react";
 
 export default function Home() {
 
-  let link = ''
+  const [link, setLink] = useState('')
+
+  const handleChange = (event: any) => {
+    setLink(event.target.value)
+  }
 
   return (
       <main className="flex flex-col justify-center items-center">
         <h1 className="mt-8 mb-20 text-3xl font-bold">Link Sharing</h1>
-        <div className="bg-amber-200 flex flex-col justify-center items-center text-black text-lg font-semibold py-6 px-5 rounded-xl w-1/2">
+        <div className="bg-amber-200 flex flex-col justify-center items-center gap-4 text-black text-lg font-semibold py-6 px-5 rounded-xl w-1/2">
           {link == '' ? <h1>No Link</h1> : <Link href={link} target="_blank"></Link>}
           <button className="bg-red-800 px-4 py-2 rounded-xl font-semibold text-amber-100 box-pop hover:bg-red-900 hover:text-amber-50 transition duration-300">Copy</button>
-          <input></input>
+          <input onChange={handleChange} placeholder='New Link' required className="bg-amber-100 w-11/12 box-pop px-4 py-2 rounded-xl font-semibold outline-none focus:ring focus:ring-gray-300 transition duration-300 sm:w-5/6 lg:w-9/12"></input>
           <button className="bg-red-800 px-4 py-2 rounded-xl font-semibold text-amber-100 box-pop hover:bg-red-900 hover:text-amber-50 transition duration-300">UPDATE</button>
         </div>
       </main>
