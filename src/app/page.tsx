15bc28e59f2db2e947c08916b819393935286f54
Brandon McConathy <brandonmcconathy4321@gmail.com>
2024-05-08 15:55:32 -0700

    Updated styles on input box and update button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,8 +27,8 @@ export default function Home() {
             <Link href={link} target="_blank">{link}</Link>
             <button className="bg-red-800 px-4 py-2 rounded-xl font-semibold text-amber-100 box-pop hover:bg-red-900 hover:text-amber-50 transition duration-300">Copy</button>
           </> }
-          <form onSubmit={handleSubmit}>
-            <input onChange={handleChange} placeholder='New Link' required className="bg-amber-100 w-11/12 box-pop px-4 py-2 rounded-xl font-semibold outline-none focus:ring focus:ring-gray-300 transition duration-300 sm:w-5/6 lg:w-9/12"></input>
+          <form onSubmit={handleSubmit} className="flex flex-col justify-center items-center gap-5">
+            <input onChange={handleChange} value={input} placeholder='New Link' required className="bg-amber-100 w-11/12 box-pop px-4 py-2 rounded-xl font-semibold outline-none focus:ring focus:ring-gray-300 transition duration-300 sm:w-5/6 lg:w-9/12"></input>
             <button className="bg-red-800 px-4 py-2 rounded-xl font-semibold text-amber-100 box-pop hover:bg-red-900 hover:text-amber-50 transition duration-300">UPDATE</button>
           </form>
         </div>
