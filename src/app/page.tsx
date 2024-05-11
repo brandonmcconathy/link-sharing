1ef9c0601ef5749ecec9279eab04c9e4a8a00e9c
Brandon McConathy <brandonmcconathy4321@gmail.com>
2024-05-10 21:45:01 -0700

    Copy button now copies link to clipboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -35,6 +35,10 @@ export default function Home() {
     await setDoc(doc(db, 'link', 'link'), {link : newLink})
   }
 
+  const copyText = () => {
+    navigator.clipboard.writeText(link)
+  }
+
   return (
     <main className="flex flex-col justify-center items-center">
       <h1 className="mt-8 mb-20 text-3xl font-bold">Link Sharing</h1>
@@ -42,7 +46,7 @@ export default function Home() {
         {link == '' ? <h1>No Link</h1> :
         <>
           <Link href={link} target="_blank">{link}</Link>
-          <button className="bg-red-800 px-4 py-2 rounded-xl font-semibold text-amber-100 box-pop hover:bg-red-900 hover:text-amber-50 transition duration-300">Copy</button>
+          <button onClick={copyText} className="bg-red-800 px-4 py-2 rounded-xl font-semibold text-amber-100 box-pop hover:bg-red-900 hover:text-amber-50 transition duration-300">Copy</button>
         </>}
         <form onSubmit={handleSubmit} className="flex flex-col justify-center items-center gap-5">
           <input onChange={handleChange} value={input} placeholder='New Link' required className="bg-amber-100 w-11/12 box-pop px-4 py-2 rounded-xl font-semibold outline-none focus:ring focus:ring-gray-300 transition duration-300 sm:w-5/6 lg:w-9/12"></input>
