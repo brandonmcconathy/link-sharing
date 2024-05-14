6191109205ac6a3b307263da2b5cee03c7a71123
Brandon McConathy <brandonmcconathy4321@gmail.com>
2024-05-14 09:54:44 -0700

    Added loading screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -44,16 +44,19 @@ export default function Home() {
     <main className="flex flex-col justify-center items-center">
       <h1 className="mt-8 mb-20 text-3xl font-bold">Link Sharing</h1>
       <div className="bg-amber-200 flex flex-col justify-center items-center gap-4 text-black text-lg font-semibold mx-4 py-6 px-5 rounded-xl w-97 md:w-3/4 lg:w-7/12 xl:w-1/2">
-        {link == '' ? <h1>No Link</h1> :
+      {loading ? <h1>Loading...</h1> :
         <>
-          <Link href={link} target="_blank">{link}</Link>
-          <button onClick={copyText} className="bg-red-800 px-4 py-2 rounded-xl font-semibold text-amber-100 box-pop hover:bg-red-900 hover:text-amber-50 transition duration-300">Copy</button>
+          {link == '' ? <h1>No Link</h1> :
+          <>
+            <Link href={link} target="_blank">{link}</Link>
+            <button onClick={copyText} className="bg-red-800 px-4 py-2 rounded-xl font-semibold text-amber-100 box-pop hover:bg-red-900 hover:text-amber-50 transition duration-300">Copy</button>
+          </>}
+          <h2 id='copy-confirm' className="text-sm -my-3 invisible">Link Copied</h2>
+          <form onSubmit={handleSubmit} className="flex flex-col justify-center items-center gap-5">
+            <input onChange={handleChange} value={input} placeholder='New Link' required className="bg-amber-100 w-11/12 box-pop px-4 py-2 rounded-xl font-semibold outline-none focus:ring focus:ring-gray-300 transition duration-300 sm:w-5/6 lg:w-9/12"></input>
+            <button className="bg-red-800 px-4 py-2 rounded-xl font-semibold text-amber-100 box-pop hover:bg-red-900 hover:text-amber-50 transition duration-300">Update</button>
+          </form>
         </>}
-        <h2 id='copy-confirm' className="text-sm -my-3 invisible">Link Copied</h2>
-        <form onSubmit={handleSubmit} className="flex flex-col justify-center items-center gap-5">
-          <input onChange={handleChange} value={input} placeholder='New Link' required className="bg-amber-100 w-11/12 box-pop px-4 py-2 rounded-xl font-semibold outline-none focus:ring focus:ring-gray-300 transition duration-300 sm:w-5/6 lg:w-9/12"></input>
-          <button className="bg-red-800 px-4 py-2 rounded-xl font-semibold text-amber-100 box-pop hover:bg-red-900 hover:text-amber-50 transition duration-300">Update</button>
-        </form>
       </div>
     </main>
   );
